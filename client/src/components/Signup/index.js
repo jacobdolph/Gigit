e9228551fd66d31162fbb378cc4fac52d5d9194c
Jacob Dolph <jdolph2@instructors.2u.com>
2022-02-05 20:33:31 -0700

    adding sign up and login features using graphql and express session

--- client/src/components/Signup/index.js
+++ client/src/components/Signup/index.js
@@ -1,11 +1,58 @@
-import React, { Component, useState } from "react";
+import { useMutation } from "@apollo/client";
+import React, { useState } from "react";
+import { ADD_USER, LOGIN_USER } from "../../utils/mutations";
 import "./style.css";
 
 function Signup() {
   const [loginState, setLoginState] = useState(false);
+  const [addUser, { error }] = useMutation(ADD_USER);
+  const [login, { err }] = useMutation(LOGIN_USER);
+  const [formState, setFormState] = useState({
+    userName: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
 
   const handleChangeLoginState = () => {
     setLoginState(!loginState);
+  };
+
+  const handleSignUpSubmit = async (event) => {
+    event.preventDefault();
+
+    // use try/catch instead of promises to handle errors
+    try {
+      // execute addUser mutation and pass in variable data from form
+      const { data } = await addUser({
+        variables: { ...formState },
+      });
+      console.log(data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  const handleLoginSubmit = async (event) => {
+    event.preventDefault();
+
+    // use try/catch instead of promises to handle errors
+    try {
+      // execute addUser mutation and pass in variable data from form
+      const { data } = await login({
+        variables: { ...formState },
+      });
+      console.log(data);
+    } catch (e) {
+      console.error(e);
+    }
   };
 
   return (
@@ -16,22 +63,57 @@ function Signup() {
       id='container'
     >
       <div className='form-container sign-up-container'>
-        <form className='signup'>
+        <form className='signup' onSubmit={handleSignUpSubmit}>
           <h1>Create Account</h1>
           <span>or use your email for registration</span>
-          <input type='text' id='signUpUsername' placeholder='Username' />
-          <input type='email' id='signUpEmail' placeholder='Email' />
-          <input type='password' id='signUpPassword' placeholder='Password' />
-          <button>Sign Up</button>
+          <input
+            type='text'
+            id='signUpUsername'
+            placeholder='Username'
+            name='userName'
+            value={formState.userName}
+            onChange={handleChange}
+          />
+          <input
+            type='email'
+            id='signUpEmail'
+            placeholder='Email'
+            name='email'
+            value={formState.email}
+            onChange={handleChange}
+          />
+          <input
+            type='password'
+            id='signUpPassword'
+            placeholder='Password'
+            name='password'
+            value={formState.password}
+            onChange={handleChange}
+          />
+          <button type='submit'>Sign Up</button>
         </form>
       </div>
       <div className='form-container sign-in-container'>
-        <form className='login'>
+        <form className='login' onSubmit={handleLoginSubmit}>
           <h1>Sign in</h1>
           <span>or use your account</span>
-          <input type='text' id='username-input' placeholder='Username' />
-          <input type='password' id='password-input' placeholder='Password' />
-          <button>Log In</button>
+          <input
+            type='text'
+            id='username-input'
+            placeholder='Email'
+            name='email'
+            value={formState.email}
+            onChange={handleChange}
+          />
+          <input
+            type='password'
+            id='password-input'
+            placeholder='Password'
+            name='password'
+            value={formState.password}
+            onChange={handleChange}
+          />
+          <button type='submit'>Log In</button>
         </form>
       </div>
       <div className='overlay-container'>
