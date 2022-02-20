f6433d5ad96bb920ef482e8f9426f75ec65dde1b
Jacob Dolph <jdolph2@instructors.2u.com>
2022-02-20 14:53:11 -0700

    trying new static files serve

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 const { auth } = require("express-openid-connect");
 const routes = require("./routes");
 const cors = require("cors");
-
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -38,8 +38,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-  app.use("/*", express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 // app.use(routes);
 
 // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/giglist", {
