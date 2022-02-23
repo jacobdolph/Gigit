32368130bc3de40f7c13c8de3c283449c62e93e9
Jacob Dolph <jdolph2@instructors.2u.com>
2022-02-22 20:12:37 -0700

    new changes

--- models/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  Gig: require("./gig"),
-  User: require("./user"),
-};
--- routes/api/index.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const gigRoutes = require("./gigs");
-
-// gig routes
-router.use("/gigs", gigRoutes);
-
-module.exports = router;
